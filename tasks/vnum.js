64342f1924de70267e58ab70aaab94b64a4779a0
Joshua Thornton <jshthornton@gmail.com>
2013-08-24 13:59:45 +0100

    Project cleanup.

---
+++ tasks/vnum.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+	// Nodejs libs.
+	var path = require('path');
+
+	// ==========================================================================
+	// TASKS
+	// ==========================================================================
+
+	grunt.registerInitTask('vnum', 'Update file version numbers from an argument.', function() {
+		
+	});
+
+};
