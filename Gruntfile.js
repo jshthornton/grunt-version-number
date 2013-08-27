32b7150ae7eea758068c2e5ddba37807fad32a32
Joshua Thornton <jshthornton@gmail.com>
2013-08-27 15:04:05 +0100

    Base working code.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,5 +4,18 @@ module.exports = function(grunt) {
 
 	// Project configuration.
 	grunt.initConfig({
+		vnum: {
+			dist: {
+				src: ['tests/test.txt'],
+				overwrite: true,
+
+				replacements: [{
+					from: /"version": ".*"/ig,
+					to: '"version": "<%= vnum.version_number %>"'
+				}]
+			}
+		}
 	});
+
+	grunt.task.loadTasks('tasks');
 };
